1e0de767c708522a0beee444aa5bb61290c3fd6e
Kevin Wang <wwang1@woolworths.com.au>
2024-01-05 22:05:16 +1100

    add player opponent

---
+++ Importers/TtPlayers.Importer/Domain/Models/PlayerOpponent.cs
@@ -0,0 +1,45 @@
+﻿using Google.Cloud.Firestore;
+
+namespace TtPlayers.Importer.Domain.Models
+{
+    [FirestoreData]
+    public class PlayerOpponent
+    {
+        [FirestoreProperty]
+        public string? Id { get; set; }
+
+        [FirestoreProperty]
+        public string? FullName { get; set; }
+
+        public List<PlayerOpponentMatch> Matches { get; set; }
+    }
+
+    [FirestoreData]
+    public class PlayerOpponentMatch
+    {
+        [FirestoreProperty]
+        public string? Id { get; set; }
+
+        [FirestoreProperty]
+        public DateTime MatchDate { get; set; }
+
+        [FirestoreProperty]
+        public string WinnerId { get; set; }
+
+        [FirestoreProperty]
+        public string LoserId { get; set; }
+
+        [FirestoreProperty]
+        public int? WinnerSetWins { get; set; }
+
+        [FirestoreProperty]
+        public List<int> WinnerSetScores { get; set; }
+
+        [FirestoreProperty]
+        public int? LoserSetWins { get; set; }
+
+        [FirestoreProperty]
+        public List<int> LoserSetScores { get; set; }
+
+    }
+}
---
+++ Importers/TtPlayers.Importer/Domain/Models/PlayerOpponentList.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Firestore;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TtPlayers.Importer.Domain.Attributes;
+
+namespace TtPlayers.Importer.Domain.Models
+{
+    [BsonCollection("PlayerOpponents")]
+    [FirebaseCollection("PlayerOpponents")]
+    [FirestoreData]
+    public class PlayerOpponentList : IDocument
+    {
+        [BsonElement("_id")]
+        [BsonId]
+        [FirestoreProperty] 
+        public string? Id { get; set; }
+
+        [FirestoreProperty]
+        public string? FullName { get; set; }
+
+        [FirestoreProperty] 
+        public List<PlayerOpponent> Opponents { get; set;}
+
+        [FirestoreProperty]
+        public DateTime LastUpdated { get; set; }
+
+        // properties to push to firebase
+        public DateTime? LastDeltaPushDate { get; set; }
+        public bool RequireDeltaPush { get; set; }
+    }
+}
